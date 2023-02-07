0d151fd0d45e65d95153db90c2d911d3f7af30e4
Joseph Y <jcy_y2000@hotmail.com>
2023-02-07 15:44:18 -0600

    For some reason, changes to documentation of assignment 1 wasn't pushed.

--- assignment001/index.php
+++ assignment001/index.php
@@ -17,18 +17,24 @@
     // Put the entire text file into a massive string, cause that's the only way I know. -_-
     $fileString = file_get_contents($relativeFilePath);
 
+    // Terminate if I can't read or find the find
     if(!$fileString) exit("Couldn't read or find the file: " + $relativeFilePath);
 
+    // The regex that we will be using, refer to notes or website for what all the symbols mean
     $courseRegex = "/(MATH\s\d+R?)(\s{3}\S[A-Z\s?:\-\&]+\s{2})(?:Repeatable\s{3})?(\d\-?\d{0,2})\s/";
 
+    // Create the table
     echo "<table class='table'>
             <tr>
                 <th>Course</th>
                 <th>Title</th>
                 <th>Credits</th>
             </tr>";
+
+    // Extract all text that matches the regex
     if(preg_match_all($courseRegex, $fileString, $courseStrings)){
         for($i=0;$i<count($courseStrings[0]);$i++) {
+            // Create a table row and put the Code, Name, and amount of credits for each class
             echo "<tr>
                     <td>{$courseStrings[1][$i]}</td>
                     <td>{$courseStrings[2][$i]}</td>
@@ -37,9 +43,9 @@
             
         }
     }
-    else {
+    else
         echo "it didn't work!";
-    }
+    // Finish the table
     echo "</table>";
 
 ?>
