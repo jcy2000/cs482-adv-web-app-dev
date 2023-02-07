0eee71e836977ba5e5857ca1dc6944be314266b9
Joseph Y <jcy_y2000@hotmail.com>
2023-02-07 15:43:51 -0600

    Created index for assignment 2. Updated documentation for assignment 1.

---
+++ assignment002/index.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<?php
+/*  Original Author: MonKey (aka Joseph Yang)
+    Code Summary: I actually have no idea at the moment, but when I do, I'll change this summary lol.
+*/
+
+?>
