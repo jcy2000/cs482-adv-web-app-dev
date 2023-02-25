080dca0e27db3eb2d899b8ee92d503f80e491540
jcy2000 <jcy_y2000@hotmail.com>
2023-02-24 20:53:45 -0600

    Added header line to conform to example.

--- assignment002/index.php
+++ assignment002/index.php
@@ -1,4 +1,5 @@
-<head>
+<head lang="en">
+    <title>Sumthing</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -17,28 +18,29 @@
     $fileString = file_get_contents($relativeFilePath);
 
     // Terminate if I can't read or find the find
-    if(!$fileString) exit("Couldn't read or find the file: " + $relativeFilePath);
+    if (!$fileString) exit("Couldn't read or find the file: " + $relativeFilePath);
 
     // The regex that we will be using, refer to notes or website for what all the symbols mean
     $courseRegex = "/([A-Z]+\s\d{3}[A-Z]?)\s(\d{2}[A-Z]?)\s\d+\s([A-Z\s\&\-\/\:]+[A-Z])\s([A-Za-z]+\s?[A-Za-z]+)\s+(\d\s?\-?\s?\d*)\s+[\w\s\:\/\-]+Instructor\:\s+([\w\,\'\-]+\s?[A-Za-z]+)/";
 
     // Create the table
-    echo "<table class='table'>
+    
+
+    // Extract all text that matches the regex
+    if (preg_match_all($courseRegex, $fileString, $courseStrings)){
+        echo "<h1>There is a total of ".count($courseStrings[0])." courses offered in summer session</h1>";
+        echo "<table class='table'>
             <tr>
-                <th>#</th>
                 <th>Course</th>
                 <th>Title</th>
                 <th>Section</th>
                 <th>Component</th>
                 <th>Credits</th>
+                <th>Instructor</th>
             </tr>";
-
-    // Extract all text that matches the regex
-    if(preg_match_all($courseRegex, $fileString, $courseStrings)){
-        for($i=0;$i<count($courseStrings[0]);$i++) {
+        for ($i=0;$i<count($courseStrings[0]);$i++) {
             // Create a table row and put the Code, Name, and amount of credits for each class
             echo "<tr>
-                    <td>$i</td>
                     <td>{$courseStrings[1][$i]}</td>
                     <td>{$courseStrings[3][$i]}</td>
                     <td>{$courseStrings[2][$i]}</td>
@@ -47,9 +49,7 @@
                     <td>{$courseStrings[6][$i]}</td>
                   </tr>";
         }
-    }
-    else
+    } else
         echo "it didn't work!";
     // Finish the table
     echo "</table>";
-?>
